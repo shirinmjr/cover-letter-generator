a3601027416caf513ed44490c552a0719558a54c
Shirin Mohajer <shirin@Shirins-MacBook-Pro.local>
2024-11-24 15:28:25 -0500

    created index.js for backend entry point

---
+++ server/src/index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const bankController = require("./controllers/bankController");
